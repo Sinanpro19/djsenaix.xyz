644918c2ca82b8650ac16e2c9d0116bf36be2f4a
SenaiX <sinanpro19@gmail.com>
2026-03-18 20:34:30 +0400

    Add files via upload

---
+++ toggle_theme.js
@@ -0,0 +1,22 @@
+// Theme toggle for dark/light mode
+function toggleTheme() {
+  const html = document.documentElement;
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  } else {
+    html.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  }
+}
+// Set default theme to dark
+if (!localStorage.getItem('theme')) {
+  document.documentElement.classList.add('dark');
+  localStorage.setItem('theme', 'dark');
+} else {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+}
